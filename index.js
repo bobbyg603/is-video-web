589abc8c88ca21b8bf7860e5cbe35db724e2de5c
Bobby Galli <bobbyg603@gmail.com>
2023-07-25 08:50:45 -0400

    fix: web compatible

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import path from 'node:path';
 import videoExtensions from 'video-extensions' assert { type: "json" };
 const exts = Object.create(null);
 
 videoExtensions.forEach(el => exts[el] = true);
 
-const isVideo = filePath => path.extname(filePath).slice(1).toLowerCase() in exts;
+const isVideo = filePath => filePath.split('.').pop().toLowerCase() in exts;
 
 export default isVideo;
